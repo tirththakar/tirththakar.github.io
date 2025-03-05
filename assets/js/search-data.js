74827a176701a69667326c68b9e7df27016f094f
tirththakar <70041873+tirththakar@users.noreply.github.com>
2025-03-05 05:59:47 +0000

    Deploying to gh-pages from @ tirththakar/tirththakar.github.io@02defa24ad87a31286a7cef8a76685517ee41320 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "resume",
           description: "",
           section: "Navigation",
           handler: () => {

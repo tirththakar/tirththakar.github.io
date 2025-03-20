c12aaaedabea782287865e83f8f1a7ba1f87e349
tirththakar <70041873+tirththakar@users.noreply.github.com>
2025-03-20 14:48:01 +0000

    Deploying to gh-pages from @ tirththakar/tirththakar.github.io@087ae0abe41cf5e1cf0a6c9dbcef5b4859e8d859 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,9 +65,9 @@ ninja.data = [{
           description: "Designing and testing the effectiveness of various materials for electroadhesion",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-soft-robots-in-extreme-environments",
+          title: 'Soft Robots in Extreme Environments',
+          description: "First author publication on the performance and appliations of soft robots under harsh conditions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",

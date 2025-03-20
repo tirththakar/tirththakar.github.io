ee5db4eeead37e8ebbdeb946b7e7bc611f4e4f4d
tirththakar <70041873+tirththakar@users.noreply.github.com>
2025-03-20 14:49:19 +0000

    Deploying to gh-pages from @ tirththakar/tirththakar.github.io@7b4a35d795755dca352594c5c3a2aead51d9d1d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,9 +70,9 @@ ninja.data = [{
           description: "First author publication on the performance and appliations of soft robots under harsh conditions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-game-controller-design",
+          title: 'Game Controller Design',
+          description: "Designing a custom game controller using CAD",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",

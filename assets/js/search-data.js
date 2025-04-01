ef869244a21364c75dd19ffd649a6ebe93bd49e7
tirththakar <70041873+tirththakar@users.noreply.github.com>
2025-04-01 15:22:58 +0000

    Deploying to gh-pages from @ tirththakar/tirththakar.github.io@81c28729d8853b0f42906c091fcc85c5817d1a8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -90,8 +90,8 @@ ninja.data = [{
           description: "Conducting FEA simulation on a ratchet wrench using Ansys",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-raspberry-pi-heatsink-thermal-analysis",
-          title: 'Raspberry Pi Heatsink Thermal Analysis',
+            },},{id: "projects-rpi-heatsink-thermal-analysis",
+          title: 'RPi Heatsink Thermal Analysis',
           description: "Designing a heatsink for a Rasperry Pi and validating its performance via thermal FEA",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";

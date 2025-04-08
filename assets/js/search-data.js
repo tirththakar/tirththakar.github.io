d195ee318a24931a6326d4740c7985f726cd0ae6
tirththakar <70041873+tirththakar@users.noreply.github.com>
2025-04-08 02:31:46 +0000

    Deploying to gh-pages from @ tirththakar/tirththakar.github.io@0944c27b5330f749a9df9465a8583bd8273fe41d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,11 +50,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-electroadhesion-pads",
-          title: 'Electroadhesion Pads',
-          description: "Designing and testing the effectiveness of various materials for electroadhesion",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{id: "projects-soft-robots-in-extreme-environments",
           title: 'Soft Robots in Extreme Environments',
           description: "First author publication on the performance and appliations of soft robots under harsh conditions",
@@ -85,6 +80,11 @@ ninja.data = [{
           description: "Designing a heatsink for a Rasperry Pi and validating its performance via thermal FEA",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-electroadhesion-pads",
+          title: 'Electroadhesion Pads',
+          description: "Designing and testing the effectiveness of various materials for electroadhesion",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/electroadhesion_pads/";
             },},{id: "projects-soft-end-effector-gripper-system",
           title: 'Soft End Effector Gripper System',
           description: "Using dielectric elastomer actuators as a soft gripper",
